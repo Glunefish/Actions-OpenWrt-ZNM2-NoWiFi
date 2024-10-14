ad2fa20f73fdd32749df0b754e8f5f9d1513d12d
kkstone <10545609+kkstone@users.noreply.github.com>
2024-10-14 14:01:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,9 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+sed -i 's/LiBwrt/OpenWrt/g' include/version.mk
+sed -i 's/LiBwrt/OpenWrt/g' package/base-files/image-config.in
+sed -i 's/LiBwrt/OpenWrt/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+rm -rf package/base-files/files/etc/banner
+wget https://github.com/openwrt/openwrt/raw/refs/heads/main/package/base-files/files/etc/banner -O package/base-files/files/etc/banner
