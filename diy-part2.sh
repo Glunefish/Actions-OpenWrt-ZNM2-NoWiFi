1a11ee527f5a3d391ea8bd173d352b68bee8b918
kkstone <10545609+kkstone@users.noreply.github.com>
2025-05-30 19:03:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,10 +9,12 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-sed -i 's/LiBwrt/OpenWrt/g' include/version.mk
-sed -i 's/LiBwrt/OpenWrt/g' package/base-files/image-config.in
-sed -i 's/LiBwrt/OpenWrt/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LibWRT/OpenWRT/g' include/version.mk
+sed -i 's/LibWRT/OpenWRT/g' config/Config-images.in
+sed -i 's/LibWRT/OpenWRT/g' package/base-files/image-config.in
+sed -i 's/LibWRT/OpenWRT/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i 's/LibWRT/OpenWRT/g' package/base-files/files/bin/config_generate
+sed -i 's/LibWRT/OpenWRT/g' target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
 rm -rf package/base-files/files/etc/banner
 wget https://github.com/openwrt/openwrt/raw/refs/heads/main/package/base-files/files/etc/banner -O package/base-files/files/etc/banner
 rm -rf target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-cpufreq.dtsi
