6a1caca2eebcb0d4147c7991083d7bdb9702016f
GruntFish <fbyu7@qq.com>
2025-08-25 12:57:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/LiBwrt/OpenWrt/g' package/base-files/image-config.in
 sed -i 's/LiBwrt/OpenWrt/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LiBwrt/OpenWrt/g' target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
-rm -rf package/base-files/files/etc/banner
-wget https://github.com/openwrt/openwrt/raw/refs/heads/main/package/base-files/files/etc/banner -O package/base-files/files/etc/banner
+# rm -rf package/base-files/files/etc/banner
+# wget https://github.com/openwrt/openwrt/raw/refs/heads/main/package/base-files/files/etc/banner -O package/base-files/files/etc/banner
 # rm -rf target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-cpufreq.dtsi
 # wget https://github.com/kkstone/Actions-OpenWrt-ZNM2-NoWiFi/raw/refs/heads/master/ipq6018-cpufreq.dtsi -O target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-cpufreq.dtsi
